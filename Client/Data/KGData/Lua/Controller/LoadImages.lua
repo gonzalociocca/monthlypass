fb0b9e6208098ce196b4da2d7449648eeb80f0dc
gonzalociocca <112786087+gonzalociocca@users.noreply.github.com>
2025-01-19 23:55:07 -0300

    Add files via upload

---
+++ Client/Data/KGData/Lua/Controller/LoadImages.lua
@@ -0,0 +1,13 @@
+--[[
+here you can upload images, I recommend using from the number 40000
+--]]
+
+function LoadImages()
+	LoadImage("Interface///Logito.jpg", 40000)
+	LoadImage("Interface///btnxBC.tga", 40001)
+    LoadImage("KGData///Lua///Manager///LoadImage///MonthlyPass///background.tga",40011)
+    LoadImage("KGData///Lua///Manager///LoadImage///MonthlyPass///bar.tga",40012)
+    LoadImage("KGData///Lua///Manager///LoadImage///MonthlyPass///blockred.tga",40013)
+    LoadImage("KGData///Lua///Manager///LoadImage///MonthlyPass///blockgreen.tga",40014)
+    LoadImage("KGData///Lua///Manager///LoadImage///MonthlyPass///blockyellow.tga",40015)
+end
